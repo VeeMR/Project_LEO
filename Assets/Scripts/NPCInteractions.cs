1bd3aaec77ab75aa9e2b72b5e38953f0e6d8817a
VeeMR <124574340+VeeMR@users.noreply.github.com>
2025-01-09 10:40:18 +0000

    Changes
    Added light
    Changed flashlight position
    Tried audio sources

--- Assets/Scripts/NPCInteractions.cs
+++ Assets/Scripts/NPCInteractions.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class NPCInteractions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   // Assign the AudioSource component in the inspector
+    public AudioSource audioSource;
+    
 
-    // Update is called once per frame
+    // Dictionary to map tags to audio clips
+    public Dictionary<string, AudioClip> tagToAudioClip;
+
     void Update()
     {
-        
+        // Initialize the dictionary (Example setup, modify as needed)
+        tagToAudioClip = new Dictionary<string, AudioClip>
+        {
+            { "Pikachu", Resources.Load<AudioClip>("Pikachu") },
+            { "Poster", Resources.Load<AudioClip>("Tech4Good") }
+        };
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the colliding object's tag is in the dictionary
+        if (tagToAudioClip.ContainsKey(collision.gameObject.tag))
+        {
+            // Play the corresponding audio clip
+            audioSource.clip = tagToAudioClip[collision.gameObject.tag];
+            audioSource.Play();
+        }
+
     }
 }
+
+
